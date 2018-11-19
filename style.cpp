91335243b4d55ec000c39233a1acbcbf7ed69fc1
Eric <ewomer@hotmail.com>
2018-11-18 20:20:17 -0500

    Added a style.h and style.txt file to the project.

--- style.cpp
+++ style.cpp
@@ -1,2 +1,26 @@
 
-#include "someinc.h"
+
+#include "style.h"
+
+void function();  // empty
+
+char* run(int val, char* valTwo, bool valThree)
+{
+    val = 1;
+    valTwo = &valOne;
+    valThree = false;
+
+    function();
+
+    return 0;
+}
+
+void function()
+{
+    NewType typeNew;
+    if (typeNew.Size() == 0) {
+        // Do something
+    } else {  // Always wrap else with braces
+              // Do something else
+    }
+}
