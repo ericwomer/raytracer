a722af3a8c81c53fed376c569dae50e0ed07f5fc
Salamanderrake <salamanderrake@gmail.com>
2018-12-08 08:40:58 -0500

    After running the new .clang-format config

--- src/base/base.h
+++ src/base/base.h
@@ -5,25 +5,24 @@
 #include <vector>
 
 class Base {
-public:  // data types
+  public:  // data types
+  // Public Interface
+  Base(){};
+  virtual ~Base(){};
+  virtual int               main()                                 = 0;
+  virtual int               main(std::vector<std::string>& params) = 0;
+  virtual int               size()                                 = 0;
+  virtual const std::string name()                                 = 0;
+  virtual void              help()                                 = 0;
+  virtual void              name(std::string name)                 = 0;
+  virtual void              version()                              = 0;
 
-    // Public Interface
-    Base(){};
-    virtual ~Base(){};
-    virtual int               main() = 0;
-    virtual int               main(std::vector<std::string>& params) = 0;
-    virtual int               size() = 0;
-    virtual const std::string name() = 0;
-    virtual void              help() = 0;
-    virtual void              name(std::string name) = 0;
-    virtual void              version() = 0;
+  protected:
+  // Protected data members
+  std::string              app_name;
+  std::vector<std::string> app_description;
 
-protected:
-    // Protected data members
-    std::string              app_name;
-    std::vector<std::string> app_description;
-
-    std::string version_number;
+  std::string version_number;
 };
 
 #endif  // BASE_H
--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,16 @@ RaytracingApp rayTracer;
 
 int main(int argc, char* argv[])
 {
-    std::vector<std::string> params;
-    for (int i = 0; i != argc; i++) {
-        params.push_back(argv[i]);
-    }
+  std::vector<std::string> params;
+  for (int i = 0; i != argc; i++) {
+    params.push_back(argv[i]);
+  }
 
-    try {
-        rayTracer.main(params);
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
+  try {
+    rayTracer.main(params);
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
