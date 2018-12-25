000a01231b07ff0668b5bb056ce5067732ae9040
Eric <ewomer@hotmail.com>
2018-12-25 03:03:48 -0500

    Removed tbb for now.

--- src/rendering/raytracer.cpp
+++ src/rendering/raytracer.cpp
@@ -1,5 +1,5 @@
-#include <tbb/parallel_for.h>
-#include <tbb/blocked_range.h>
+// #include <tbb/parallel_for.h>
+// #include <tbb/blocked_range.h>
 
 #include <thread>
 #include <future>
@@ -8,46 +8,46 @@
 #include "ray.h"
 // Vec3 sample(
 
-struct sample_s {
-  Vec3&     _col;
-  int       _i, _j;
-  camera*   _cam;
-  Vec2<int> _res;
-  hitable*  _world;
-
-  sample_s(sample_s const& other)
-      : _i(other._i)
-      , _j(other._j)
-      , _cam(other._cam)
-      , _res(other._res)
-      , _col(other._col)
-      , _world(other._world)
-
-  {
-  }
-  sample_s(int i, int j, camera* cam, Vec2<int>& res, hitable* world, Vec3& col)
-      : _i(i)
-      , _j(j)
-      , _cam(cam)
-      , _res(res)
-      , _col(col)
-      , _world(world)
-
-  {
-  }
-
-  Vec3 operator()(const tbb::blocked_range<size_t>& range) const
-  {
-    std::cout << "Begin: " << range.begin() << " End: " << range.end() << "\n";
-    for (size_t s = range.begin(); s != range.end(); ++s) {
-      std::cout << s << "\n";
-      double u = double(_i + drand48()) / double(_res.x());
-      double v = double(_j + drand48()) / double(_res.y());
-      ray    r = _cam->get_ray(u, v);
-      _col += color(r, _world, 0);
-    }
-  }
-};
+//struct sample_s {
+//  Vec3&     _col;
+//  int       _i, _j;
+//  camera*   _cam;
+//  Vec2<int> _res;
+//  hitable*  _world;
+//
+//  sample_s(sample_s const& other)
+//      : _i(other._i)
+//      , _j(other._j)
+//      , _cam(other._cam)
+//      , _res(other._res)
+//      , _col(other._col)
+//      , _world(other._world)
+//
+//  {
+//  }
+//  sample_s(int i, int j, camera* cam, Vec2<int>& res, hitable* world, Vec3& col)
+//      : _i(i)
+//      , _j(j)
+//      , _cam(cam)
+//      , _res(res)
+//      , _col(col)
+//      , _world(world)
+//
+//  {
+//  }
+//
+//  Vec3 operator()(const tbb::blocked_range<size_t>& range) const
+//  {
+//    std::cout << "Begin: " << range.begin() << " End: " << range.end() << "\n";
+//    for (size_t s = range.begin(); s != range.end(); ++s) {
+//      std::cout << s << "\n";
+//      double u = double(_i + drand48()) / double(_res.x());
+//      double v = double(_j + drand48()) / double(_res.y());
+//      ray    r = _cam->get_ray(u, v);
+//      _col += color(r, _world, 0);
+//    }
+//  }
+//};
 
 // Eric: Rewrite this so it renders to the screen instead of blotting to it. like, takes in an
 // object that can write the pixels to the screen after the sample bit maybe.
