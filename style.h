9217cc0eb03d632169ed4ee59e0fa8efdc5e8ecb
Eric <ewomer@hotmail.com>
2018-12-25 03:03:05 -0500

    Formatting with clang-format.sh

--- style.h
+++ style.h
@@ -2,32 +2,32 @@
 #define STYLE_H
 
 class Base {
-public:
-    RootType(){};
-    virtual ~RootType() = 0;
-    virtual int MethodOne() = 0;
-    virtual int Size() { return sizeof(this); }
+  public:
+  RootType(){};
+  virtual ~RootType()     = 0;
+  virtual int MethodOne() = 0;
+  virtual int Size() { return sizeof(this); }
 
-protected:
-private:
-    int variableOne;
-    int variableTwo;
+  protected:
+  private:
+  int variableOne;
+  int variableTwo;
 };
 
 class Derived : public Base {
-public:
-    NewType() {}
-    NewType(int var)
-        : variableOne(var){};
-    ~virtual NewType() {}
-    virtual in  MethodOne();
-    virtual int Size() { return sizeof(this); }
+  public:
+  NewType() {}
+  NewType(int var)
+      : variableOne(var){};
+  ~virtual NewType() {}
+  virtual in  MethodOne();
+  virtual int Size() { return sizeof(this); }
 
-protected:
-private:
-    int   variableOne;
-    int   variableTwo;
-    char* pointerVariableOne;
+  protected:
+  private:
+  int   variableOne;
+  int   variableTwo;
+  char* pointerVariableOne;
 };
 
 #endif  // STYPLE_H
