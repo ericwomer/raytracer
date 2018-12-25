7c7f07e066b9e5500263975143da43e40730237f
Eric <ewomer@hotmail.com>
2018-12-25 02:59:55 -0500

    No hardcoding paths to clang-format

--- clang-format.sh
+++ clang-format.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/clang-format -i -style=file -verbose `find . -type f \( -name '*.h' -o -name '*.cpp' -o -name '*.inl' \) -not -path "./thirdparty/*" ` "$@"
+clang-format -i -style=file -verbose `find . -type f \( -name '*.h' -o -name '*.cpp' -o -name '*.inl' \) -not -path "./src/thirdparty/*" ` "$@"
 
